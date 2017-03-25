771e0eff5ccef873feb8fc1c39b739f18b0647dc
Khasir Hean <trraa@hotmail.com>
2017-03-25 16:19:12 -0400

    Added Course class Modified Instructor and Student classes

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Khasir on Mar 25/17.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    boolean fulltime;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+        this.fulltime = fulltime;
     }
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course course){
+        if (courseList == null)
+            courseList = new ArrayList<Course>();
+
+        courseList.add(course);
+
     }
 }
